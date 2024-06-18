c525cbd1d7f408b59e5e765c0d0336047c343f2c
svivek19 <svivek030503@gmail.com>
2024-06-18 13:25:54 +0530

    fix; assets folder

--- src/Qsis-Components/Navbar.jsx
+++ src/Qsis-Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import logo from "../assets/company-logo.png";
+import logo from "../../public/assets/company-logo.png";
 import { Link } from "react-router-dom";
 import { CiMenuFries } from "react-icons/ci";
 
