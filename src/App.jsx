69edab3f7f2436912d23a8b640b2b4bfb0f60a5a
Mukilan K <mukilankumar003@gmail.com>
2024-06-18 14:31:18 +0530

    add: ourwork

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Qsis-Components/Navbar";
 import Footer from "./Qsis-Components/Footer";
+import Home from "./Qsis-Components/Home"
 import ContactForm from "./Qsis-Pages/Contact";
 
 const App = () => {
@@ -11,7 +12,7 @@ const App = () => {
         <Navbar />
         <main className="flex-grow">
           <Routes>
-            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/" element={<Home />} />
             {/* <Route path="/projects" element={<Projects />} /> */}
             <Route path="/contact" element={<ContactForm />} />
             {/* <Route path="/career" element={<Career />} /> */}
