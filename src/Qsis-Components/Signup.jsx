e83b023df982343e3868331aab1c4567d2fd5658
svivek19 <svivek030503@gmail.com>
2024-06-19 15:39:36 +0530

    signup page finished

--- src/Qsis-Components/Signup.jsx
+++ src/Qsis-Components/Signup.jsx
@@ -1,70 +1,106 @@
-// import React from 'react'
-import { useState } from "react"
-import {Link} from "react-router-dom"
-import axios from "axios"
-
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import adminImg from "../assets/admin.png";
 
 const Signup = () => {
-    const [name , setName] = useState("")
-    const [email , setEmail] = useState("")
-    const [passwaord , setPassword] = useState("")
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [passwaord, setPassword] = useState("");
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      axios.post("http://localhost:3001/register", {name, email, passwaord})
-      .then(result => console.log(result))
-      .catch(err => console.log(err))
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3001/register", { name, email, passwaord })
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  };
   return (
-    <div className="bg-[#17C6D4] w-full  flex justify-center px-3 py-6">
-    <div className="bg-white px-10 py-5 rounded">
-    <form  className=" " onSubmit={handleSubmit}>
-    <h2 className="text-center">
-     <strong className="text-3xl font-bold">Admin Access</strong> </h2>
+    <div className="w-full grid grid-cols-1 md:grid-cols-2 px-3 py-6">
+      <div className="px-10 py-5 ">
+        <form
+          className="w-full md:w-9/12 mx-auto mb-4 md:mb-0"
+          onSubmit={handleSubmit}
+        >
+          <div className="text-center">
+            <h1 className="text-3xl font-semibold">Admin Access</h1>{" "}
+          </div>
 
-    <div className="flex flex-col py-3">
-    <label htmlFor="name" className="py-2">
-      <strong>Name : </strong>
-    </label>
-    <input className="border border-black rounded px-2 py-1" type="text" id="name" name="name" placeholder="Your Name" required
-    onChange={(e) => setName(e.target.value)}
-    value={name}
-    />
-    </div>
+          <div className="flex flex-col py-3">
+            <label htmlFor="name" className="py-2 font-medium">
+              Name
+            </label>
+            <input
+              className="border border-slate-400 rounded-md px-2 py-1"
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Your Name"
+              required
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
+          </div>
 
-    <div className="flex flex-col py-3">
-    <label htmlFor="email" className="py-2">
-      <strong>Email : </strong>
-    </label>
-    <input className="border border-black rounded px-2 py-1" type="text" id="email" name="email" placeholder="Entter Email" required 
-    onChange={(e) => setEmail(e.target.value)}
-    value={email}
-    />
-    </div>
+          <div className="flex flex-col py-3">
+            <label htmlFor="email" className="py-2 font-medium">
+              Email
+            </label>
+            <input
+              className="border border-slate-400 rounded-md px-2 py-1"
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Entter Email"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </div>
 
-    <div className="flex flex-col py-3">
-    <label htmlFor="password" className="py-2">
-      <strong>Password : </strong>
-    </label>
-    <input className="border border-black rounded px-2 py-1" type="password" id="password" name="password" placeholder="Enter password" required
-    onChange={(e) => setPassword(e.target.value)}
-    value={passwaord}
-    />
-    </div>
+          <div className="flex flex-col py-3">
+            <label htmlFor="password" className="py-2 font-medium">
+              Password
+            </label>
+            <input
+              className="border border-slate-400 rounded-md px-2 py-1"
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+              value={passwaord}
+            />
+          </div>
 
-    <button type="submit" className="bg-green-700 w-full py-2 my-2 text-white rounded">Create Access</button>
-    </form>
+          <button
+            type="submit"
+            className="bg-emerald-500 hover:bg-emerald-600 transition duration-300 w-full py-2 my-2 text-black rounded-md uppercase font-medium"
+          >
+            Create Access
+          </button>
+        </form>
 
-    {/* <p className="italic text-sm">Already Have an Account</p>
+        {/* <p className="italic text-sm">Already Have an Account</p>
 
     <Link to={"/login"} className="">
       <button className="bg-blue-700 w-full  py-2 my-2 text-white rounded">Login</button>
       
       </Link> */}
-
       </div>
-  </div>
-  )
-}
 
-export default Signup
+      {/* image */}
+      <div className="flex justify-center w-full">
+        <img
+          src={adminImg}
+          alt="admin image"
+          className="w-[80%] object-contain"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
