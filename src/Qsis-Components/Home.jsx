1b4bd25a90a52cb41e17bff3861f701808a10896
svivek19 <svivek030503@gmail.com>
2024-06-19 13:02:12 +0530

    fix: colors card bg animation

--- src/Qsis-Components/Home.jsx
+++ src/Qsis-Components/Home.jsx
@@ -1,10 +1,9 @@
 import { FaArrowRightLong, FaMicrochip } from "react-icons/fa6";
-import { IoIosApps } from "react-icons/io";
-import { FaBrain } from "react-icons/fa";
-import { BiSolidMicrochip } from "react-icons/bi";
+
 import { Link } from "react-router-dom";
 import ImagesCarousel from "./ImagesCarousel";
 import MissionVision from "./MissionVision";
+import Service from "./Service";
 
 const Home = () => {
   return (
@@ -13,52 +12,7 @@ const Home = () => {
       <ImagesCarousel />
 
       {/* Our services section */}
-
-      <div className="my-16 justify-center space-y-8">
-        <h1 className="text-4xl font-semibold">Our Services</h1>
-        <hr className="w-14 border-2 border-sky-600" />
-      </div>
-      <div className="service-con">
-        <div className="service-card">
-          <FaMicrochip className="service-icon" size={50} />
-          <h4 className="text-xl font-semibold mb-2">AI / ML</h4>
-          <p className="font-light">
-            Our mission is to leverage technology to create a safer, more
-            efficient world. We strive to deliver top-notch solutions tailored
-            to the unique needs of our clients.
-          </p>
-        </div>
-
-        <div className="service-card">
-          <IoIosApps className="service-icon" size={50} />
-          <h4 className="text-xl font-semibold mb-2">App Development</h4>
-          <p className="font-light">
-            Our mission is to leverage technology to create a safer, more
-            efficient world. We strive to deliver top-notch solutions tailored
-            to the unique needs of our clients.
-          </p>
-        </div>
-
-        <div className="service-card">
-          <BiSolidMicrochip className="service-icon" size={50} />
-          <h4 className="text-xl font-semibold mb-2">Embedded System</h4>
-          <p className="font-light">
-            Our mission is to leverage technology to create a safer, more
-            efficient world. We strive to deliver top-notch solutions tailored
-            to the unique needs of our clients.
-          </p>
-        </div>
-
-        <div className="service-card">
-          <FaBrain className="service-icon" size={50} />
-          <h4 className="text-xl font-semibold mb-2">Innovative Solutions</h4>
-          <p className="font-light">
-            Our mission is to leverage technology to create a safer, more
-            efficient world. We strive to deliver top-notch solutions tailored
-            to the unique needs of our clients.
-          </p>
-        </div>
-      </div>
+      <Service />
 
       {/* carousal section */}
       <MissionVision />
@@ -67,7 +21,7 @@ const Home = () => {
       <div className="project-list-con service-container">
         <div className="my-16 justify-center space-y-8">
           <h1 className="text-4xl font-semibold">Our Work</h1>
-          <hr className="w-14 border-2 border-sky-600" />
+          <hr className="w-14 border-2 border-emerald-500-600" />
         </div>
         <ul className="home-project-list">
           <li className="project-card bg-emerald-500 text-white">
@@ -76,7 +30,9 @@ const Home = () => {
           <li className="project-card border-2 text-emerald-500 border-emerald-500">
             Lane Change Detection
           </li>
-          <li className="project-card bg-emerald-500 text-white">Number Plate Extractor</li>
+          <li className="project-card bg-emerald-500 text-white">
+            Number Plate Extractor
+          </li>
           <li className="project-card border-2 text-emerald-500 border-emerald-500">
             Object Detector
           </li>
