c1d1b1c928d521978093b0fb9678ce99ad7817cd
svivek19 <svivek030503@gmail.com>
2024-06-19 15:20:45 +0530

    dashboard section finish

--- src/Qsis-Components/Dashboard.jsx
+++ src/Qsis-Components/Dashboard.jsx
@@ -1,40 +1,56 @@
-// import React from 'react'
-import {Link} from "react-router-dom"
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/company-logo.png";
+import Typewriter from "typewriter-effect/dist/core";
 
 const Dashboard = () => {
+  useEffect(() => {
+    const typewriter = new Typewriter("#typewriter", {
+      strings: "Quantum Sharq Innovative Solutions",
+      autoStart: true,
+    });
+  }, []);
+
   return (
-    <div className="w-full mb-0 h-1/2 bg-cyan-300 py-10 px-6">
-    <h2 className="text-3xl px-2 py-4 font-bold text-center">Welcome, <span className="text-green-800">Quantum Sharq Innovative Solutions</span> </h2>
-  <nav>
-      <ul className=" items-center flex justify-center  gap-4 font-bold text-xl px-6 py-10 capitalize">
-        
-          <Link to={"/signup"} className="">
-              <li className="bg-[#17C6D4] px-3 py-1 rounded-md cursor-pointer">Give Access</li>
+    <div className="w-full mb-0 h-1/2  py-10 px-6">
+      <div className="flex justify-center">
+        <img src={logo} alt="qsis logo" className="w-[150px]" />
+      </div>
+
+      <h2 className="text-3xl px-2 py-4 font-bold text-center">
+        Welcome, <span id="typewriter" className="gradient-text"></span>{" "}
+      </h2>
+      <nav>
+        <ul className=" items-center flex flex-col md:flex-row justify-center gap-4 font-medium text-lg  py-10 capitalize">
+          <Link to={"/signup"} className="w-full md:w-fit">
+            <li className="bg-emerald-500 px-3 py-1 rounded-md cursor-pointer w-full text-center">
+              Give Access
+            </li>
           </Link>
 
           {/* <Link to={'/viewAccess'} className="">
               <li className="bg-[#17C6D4] px-3 py-1 rounded-md cursor-pointer">View Access</li>
           </Link> */}
 
-          <Link to={"/viewEnquiries"} className="">
-              <li className="bg-[#17C6D4] px-3 py-1 rounded-md cursor-pointer">Edit Careers</li>
+          <Link to={"/viewEnquiries"} className="w-full md:w-fit">
+            <li className="bg-emerald-500 px-3 py-1 rounded-md cursor-pointer w-full text-center">
+              Edit Careers
+            </li>
           </Link>
 
-          <Link to={"/viewEnquiries"} className="">
-              <li className="bg-[#17C6D4] px-3 py-1 rounded-md cursor-pointer">Edit Products</li>
+          <Link to={"/viewEnquiries"} className="w-full md:w-fit">
+            <li className="bg-emerald-500 px-3 py-1 rounded-md cursor-pointer w-full text-center">
+              Edit Products
+            </li>
           </Link>
 
           {/* <Link to={'/signup'} className="">
               <li className="bg-[#17C6D4] px-3 py-1 rounded-md cursor-pointer">Post Jobs</li>
           </Link> */}
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
-         
-
-
-      </ul>
-  </nav>
-</div>
-  )
-}
-
-export default Dashboard
+export default Dashboard;
